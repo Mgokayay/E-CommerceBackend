d9a43246f55b5e1ebb1edb9dff3193eaf18357b0
Mgokayay <aymustafagokay@gmail.com>
2024-03-25 14:00:26 +0300

    product entity created

---
+++ src/main/java/com/workintech/ecommerce/entity/Product.java
@@ -0,0 +1,53 @@
+package com.workintech.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "products",schema = "fsweb")
+public class Product {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "product_name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price")
+    private double price;
+
+    @Column(name = "stock")
+    private long stock;
+
+    @Column(name = "images")
+    private String images;
+
+    @Column(name = "rating")
+    private double rating;
+
+    @Column(name = "store_id")
+    private long storeId;
+
+    @Column(name = "sell_count")
+    private long sellCount;
+
+    @ManyToOne(cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+    @JoinColumn(name = "category_id")
+    Categories categories;
+
+    @ManyToOne(cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+    @JoinColumn(name = "store_id")
+    Store store;
+
+
+}
