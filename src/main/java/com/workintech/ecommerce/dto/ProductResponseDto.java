16a978027a010ba69507be0cd2cbf539c1d94798
Mgokayay <aymustafagokay@gmail.com>
2024-03-25 14:02:48 +0300

    product dto created

---
+++ src/main/java/com/workintech/ecommerce/dto/ProductResponseDto.java
@@ -0,0 +1,4 @@
+package com.workintech.ecommerce.dto;
+
+public record ProductResponseDto(Long id,String name,String description,double price,Long stock,String images,double rating,Long storeId,Long sellCount) {
+}
