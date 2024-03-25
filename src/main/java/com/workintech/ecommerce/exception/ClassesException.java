d73d953ea35c6c1dba74ff1e2a39b72f2a5982f7
Mgokayay <aymustafagokay@gmail.com>
2024-03-25 17:26:26 +0300

    all class exception managed by one exception page

--- src/main/java/com/workintech/ecommerce/exception/ClassesException.java
+++ src/main/java/com/workintech/ecommerce/exception/ClassesException.java
@@ -3,12 +3,12 @@ package com.workintech.ecommerce.exception;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 @Getter
-public class CategoriesException extends RuntimeException{
+public class ClassesException extends RuntimeException{
 
     private HttpStatus httpStatus;
 
-    public CategoriesException(String message, HttpStatus httpStatus) {
+    public ClassesException(String message,HttpStatus httpStatus){
         super(message);
-        this.httpStatus = httpStatus;
+        this.httpStatus=httpStatus;
     }
 }
