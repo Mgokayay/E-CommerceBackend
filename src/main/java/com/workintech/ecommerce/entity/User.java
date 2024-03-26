6c5e0b85564ef0804cd0f72cc058654592e5ebd0
Mgokayay <aymustafagokay@gmail.com>
2024-03-26 17:16:18 +0300

    user entity created

---
+++ src/main/java/com/workintech/ecommerce/entity/User.java
@@ -0,0 +1,37 @@
+package com.workintech.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user",schema = "fsweb")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "role_id")
+    private long roleId;
+
+//    @Column(name = "store_id")
+//    private long storeId;
+
+    @ManyToOne(cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.REFRESH,CascadeType.PERSIST})
+    @JoinColumn(name = "store_id")
+    Store store;
+}
