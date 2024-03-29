51bbfd0781622c26cea561205ffce74a7722aec2
Mgokayay <aymustafagokay@gmail.com>
2024-03-29 21:34:04 +0300

    address response dto created

---
+++ src/main/java/com/workintech/ecommerce/dto/AddressResponseDto.java
@@ -0,0 +1,4 @@
+package com.workintech.ecommerce.dto;
+
+public record AddressResponseDto(Long id,String title,String name,Long phone,String city,String district,String neighborhood,String address) {
+}
