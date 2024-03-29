557ae5fb4c74a409d8b341ab6cf4a57a6e2267d9
Mgokayay <aymustafagokay@gmail.com>
2024-03-29 21:32:10 +0300

    address entity created

---
+++ src/main/java/com/workintech/ecommerce/entity/Address.java
@@ -0,0 +1,43 @@
+package com.workintech.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "address",schema = "fsweb")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "phone")
+    private long phone;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "district")
+    private String district;
+
+    @Column(name = "neighborhood")
+    private String neighborhood;
+
+    @Column(name = "address")
+    private String address;
+
+    @ManyToOne(cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+    @JoinColumn(name = "user_id")
+    User user;
+}
